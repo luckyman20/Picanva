a2ff14f2ec40de17de1a8ba242690bc18e28e609
Sujay Kumar <official.sujaykumar@gmail.com>
2019-03-05 01:43:36 +0530

    Completed the header and added smoth scrolling

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 $(document).ready(function() {
   $(".sidenav").sidenav();
   $(".dropdown-trigger").dropdown();
-  $(".carousel.carousel-slider").carousel({
-    fullWidth: true
+  $(".carousel").carousel({
+    numVisible: 5
   });
+});
+
+function scrollToElement(obj) {
+  $("html, body").animate(
+    {
+      scrollTop: obj.offset().top
+    },
+    2000
+  );
+}
+
+$("a[href*=#]:not([href=#])").click(function() {
+  if (
+    location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") ||
+    location.hostname == this.hostname
+  ) {
+    var target = $(this.hash);
+    target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+    if (target.length) {
+      $("html,body").animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1000
+      );
+      return false;
+    }
+  }
 });
