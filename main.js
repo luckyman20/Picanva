1fc1338d23a31966b46584dfc8aa1234cd6e0685
Sujay Kumar <official.sujaykumar@gmail.com>
2019-03-10 11:13:13 +0530

    Added underline on hover to the nav links

--- main.js
+++ main.js
@@ -12,18 +12,3 @@ $('.carousel').carousel();
 setInterval(function () {
   $('.carousel').carousel('next');
 }, 5000);
-
-// window.onscroll = function () {
-//   myFunction();
-// };
-// function myFunction() {
-//   var navTag = document.getElementById("nav-tag");
-//   if (
-//     document.body.scrollTop > 100 ||
-//     document.documentElement.scrollTop > 100
-//   ) {
-//     navTag.className = "white";
-//   } else {
-//     navTag.className = "transparent z-depth-0";
-//   }
-// }
