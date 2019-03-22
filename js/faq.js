1ef13739bbc200b5b7d247673f7880aea6e392d9
Sujay Kumar <official.sujaykumar@gmail.com>
2019-03-22 17:19:35 +0530

    redundant code removal

--- js/faq.js
+++ js/faq.js
@@ -1,68 +1,33 @@
 $(document).ready(function () {
     $('.collapsible').collapsible();
-    $('.scrollspy').scrollSpy({
-        scrollOffset: 200,
-    });
+});
 
-    // For mouse devices
-
-    // $('#serv-sel').click(function () {
-    //     $('#sub-head').text('Services');
-    //     $('#services').css("display", "block");
-    //     $('#del').css("display", "none");
-    //     $('#rtrn').css("display", "none");
-    //     $('#pay').css("display", "none");
-    // })
-    // $('#del-sel').click(function () {
-    //     $('#sub-head').text('Deliver & Pick-up');
-    //     $('#services').css("display", "none");
-    //     $('#del').css("display", "block");
-    //     $('#rtrn').css("display", "none");
-    //     $('#pay').css("display", "none");
-    // })
-    // $('#rtrn-sel').click(function () {
-    //     $('#sub-head').text('Return & Refund');
-    //     $('#services').css("display", "none");
-    //     $('#del').css("display", "none");
-    //     $('#rtrn').css("display", "block");
-    //     $('#pay').css("display", "none");
-    // })
-    // $('#pay-sel').click(function () {
-    //     $('#sub-head').text('Payment & Billing');
-    //     $('#services').css("display", "none");
-    //     $('#del').css("display", "none");
-    //     $('#rtrn').css("display", "none");
-    //     $('#pay').css("display", "block");
-    // })
-
-    // For touch devices
-
-    $('#serv-sel').on('click', function () {
-        $('#sub-head').text('Services');
-        $('#services').css("display", "block");
-        $('#del').css("display", "none");
-        $('#rtrn').css("display", "none");
-        $('#pay').css("display", "none");
-    })
-    $('#del-sel').on('click', function () {
-        $('#sub-head').text('Deliver & Pick-up');
-        $('#services').css("display", "none");
-        $('#del').css("display", "block");
-        $('#rtrn').css("display", "none");
-        $('#pay').css("display", "none");
-    })
-    $('#rtrn-sel').on('click', function () {
-        $('#sub-head').text('Return & Refund');
-        $('#services').css("display", "none");
-        $('#del').css("display", "none");
-        $('#rtrn').css("display", "block");
-        $('#pay').css("display", "none");
-    })
-    $('#pay-sel').on('click', function () {
-        $('#sub-head').text('Payment & Billing');
-        $('#services').css("display", "none");
-        $('#del').css("display", "none");
-        $('#rtrn').css("display", "none");
-        $('#pay').css("display", "block");
-    })
+$('#serv-sel').on('click', function () {
+    $('#sub-head').text('Services');
+    $('#services').css("display", "block");
+    $('#del').css("display", "none");
+    $('#rtrn').css("display", "none");
+    $('#pay').css("display", "none");
+})
+$('#del-sel').on('click', function () {
+    $('#sub-head').text('Deliver & Pick-up');
+    $('#services').css("display", "none");
+    $('#del').css("display", "block");
+    $('#rtrn').css("display", "none");
+    $('#pay').css("display", "none");
+})
+$('#rtrn-sel').on('click', function () {
+    $('#sub-head').text('Return & Refund');
+    $('#services').css("display", "none");
+    $('#del').css("display", "none");
+    $('#rtrn').css("display", "block");
+    $('#pay').css("display", "none");
+})
+$('#pay-sel').on('click', function () {
+$('#sub-head').text('Payment & Billing');
+$('#services').css("display", "none");
+$('#del').css("display", "none");
+$('#rtrn').css("display", "none");
+$('#pay').css("display", "block");
+})
 });
