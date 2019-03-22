e15625362cc249ec18e5701ae06d0b7d6c867cae
Sujay Kumar <official.sujaykumar@gmail.com>
2019-03-22 22:15:52 +0530

    faq.js updated

--- js/faq.js
+++ js/faq.js
@@ -1,33 +1,31 @@
 $(document).ready(function () {
-    $('.collapsible').collapsible();
-});
 
-$('#serv-sel').on('click', function () {
-    $('#sub-head').text('Services');
-    $('#services').css("display", "block");
-    $('#del').css("display", "none");
-    $('#rtrn').css("display", "none");
-    $('#pay').css("display", "none");
-})
-$('#del-sel').on('click', function () {
-    $('#sub-head').text('Deliver & Pick-up');
-    $('#services').css("display", "none");
-    $('#del').css("display", "block");
-    $('#rtrn').css("display", "none");
-    $('#pay').css("display", "none");
-})
-$('#rtrn-sel').on('click', function () {
-    $('#sub-head').text('Return & Refund');
-    $('#services').css("display", "none");
-    $('#del').css("display", "none");
-    $('#rtrn').css("display", "block");
-    $('#pay').css("display", "none");
-})
-$('#pay-sel').on('click', function () {
-$('#sub-head').text('Payment & Billing');
-$('#services').css("display", "none");
-$('#del').css("display", "none");
-$('#rtrn').css("display", "none");
-$('#pay').css("display", "block");
-})
+    $('#serv-sel').on('click', function () {
+        $('#sub-head').text('Services');
+        $('#services').css("display", "block");
+        $('#del').css("display", "none");
+        $('#rtrn').css("display", "none");
+        $('#pay').css("display", "none");
+    })
+    $('#del-sel').on('click', function () {
+        $('#sub-head').text('Deliver & Pick-up');
+        $('#services').css("display", "none");
+        $('#del').css("display", "block");
+        $('#rtrn').css("display", "none");
+        $('#pay').css("display", "none");
+    })
+    $('#rtrn-sel').on('click', function () {
+        $('#sub-head').text('Return & Refund');
+        $('#services').css("display", "none");
+        $('#del').css("display", "none");
+        $('#rtrn').css("display", "block");
+        $('#pay').css("display", "none");
+    })
+    $('#pay-sel').on('click', function () {
+        $('#sub-head').text('Payment & Billing');
+        $('#services').css("display", "none");
+        $('#del').css("display", "none");
+        $('#rtrn').css("display", "none");
+        $('#pay').css("display", "block");
+    })
 });
