a09212ec30e74b251bc90bc1b5b4cb39b56084bf
Sujay Kumar <official.sujaykumar@gmail.com>
2019-03-24 09:38:34 +0530

    minor updates

---
+++ js/about.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#team1').click(function () {
+        $('#emp1').text("Name1");
+    });
+    $('#team2').mouseover(function () {
+        $('#team2 p').text("Name2");
+    });
+});
