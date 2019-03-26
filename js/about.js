226e500e500729adf549865fe64ebe73503ba4e3
Sujay Kumar <official.sujaykumar@gmail.com>
2019-03-26 23:14:37 +0530

    about page hover functionality added

--- js/about.js
+++ js/about.js
@@ -1,8 +1,55 @@
 $(document).ready(function () {
-    $('#team1').click(function () {
-        $('#emp1').text("Name1");
+    $('#team1').hover(function () {
+        $('#team-1').addClass('d-block');
+        $('#team-1').removeClass('d-none');
+        $('#team-2').addClass('d-none');
+        $('#team-2').removeClass('d-block');
+        $('#team-3').addClass('d-none');
+        $('#team-3').removeClass('d-block');
     });
-    $('#team2').mouseover(function () {
-        $('#team2 p').text("Name2");
+
+    $('#team2').hover(function () {
+        $('#team-1').addClass('d-none');
+        $('#team-1').removeClass('d-block');
+        $('#team-2').addClass('d-block');
+        $('#team-2').removeClass('d-none');
+        $('#team-3').addClass('d-none');
+        $('#team-3').removeClass('d-block');
+    });
+    $('#team3').hover(function () {
+        $('#team-1').addClass('d-none');
+        $('#team-1').removeClass('d-block');
+        $('#team-2').addClass('d-none');
+        $('#team-2').removeClass('d-block');
+        $('#team-3').addClass('d-block');
+        $('#team-3').removeClass('d-none');
+    });
+
+    // Advisors
+
+    $('#adv1').hover(function () {
+        $('#adv-1').addClass('d-block');
+        $('#adv-1').removeClass('d-none');
+        $('#adv-2').addClass('d-none');
+        $('#adv-2').removeClass('d-block');
+        $('#adv-3').addClass('d-none');
+        $('#adv-3').removeClass('d-block');
+    });
+
+    $('#adv2').hover(function () {
+        $('#adv-1').addClass('d-none');
+        $('#adv-1').removeClass('d-block');
+        $('#adv-2').addClass('d-block');
+        $('#adv-2').removeClass('d-none');
+        $('#adv-3').addClass('d-none');
+        $('#adv-3').removeClass('d-block');
+    });
+    $('#adv3').hover(function () {
+        $('#adv-1').addClass('d-none');
+        $('#adv-1').removeClass('d-block');
+        $('#adv-2').addClass('d-none');
+        $('#adv-2').removeClass('d-block');
+        $('#adv-3').addClass('d-block');
+        $('#adv-3').removeClass('d-none');
     });
 });
