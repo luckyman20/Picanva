360701961bb3ec5b194c0038f24589a6e4641e7a
Sujay Kumar <official.sujaykumar@gmail.com>
2019-04-03 18:47:12 +0530

    updated how page

--- js/how.js
+++ js/how.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
     $('.parallax').parallax();
+    $('.pushpin').pushpin();
+
+    $('.pushpin-head').each(function () {
+        var $this = $(this);
+        var $target = $('#' + $(this).attr('data-target'));
+        $this.pushpin({
+            top: $target.offset().top,
+            bottom: $target.offset().top + $target.outerHeight() - $this.height()
+        });
+    });
 });
