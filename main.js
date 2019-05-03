d769e70401d87df74458417f5e1f57c95aa91d4a
Chirag Gupta <guptachirag2002@gmail.com>
2019-05-03 16:50:25 +0530

    form submit

--- main.js
+++ main.js
@@ -16,3 +16,40 @@ $('.carousel').carousel();
 setInterval(function () {
   $('.carousel').carousel('next');
 }, 7000);
+
+function sendMail() {
+  var value = "First Name: " + document.getElementById('first_name').value
+  + "\n Last Name: " + document.getElementById('last_name').value
+  + "\n Email: " + document.getElementById('email').value 
+  + "\n State: " + document.getElementById('state').value
+  + "\n Phone Number: " + document.getElementById('phone').value
+  + "\n Message: " + document.getElementById('msg').value;
+  xmlhttp = new XMLHttpRequest();
+  var API_KEY = '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';
+  var url = 'https://api.sendgrid.com/v3/mail/send';
+  xmlhttp.open("POST", url, true);
+  xmlhttp.setRequestHeader("Authorization", "Bearer " + API_KEY);
+  var body = {
+    "personalizations": [
+      {
+        "to": [
+          {
+            "email": "guptachirag2002@gmail.com"
+          }
+        ],
+        "subject": "Ding Dong! Another Consultation Request"
+      }
+    ],
+    "from": {
+      "email": "gchirag@apache.org"
+    },
+    "content": [
+      {
+        "type": "text/plain",
+        "value": value
+      }
+    ] 
+  };
+  xmlhttp.send(body);
+  return false;
+}
